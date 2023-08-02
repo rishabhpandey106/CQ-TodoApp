5f84b4fedbc5d06c7b491cd23ad0e653f9b21434
rishabhpandey106 <rishabhpandey230@gmail.com>
2023-08-02 21:33:22 +0530

    temp_user

--- models/db.js
+++ models/db.js
@@ -2,5 +2,5 @@ const mongoose = require('mongoose');
 
 module.exports.init = async function () 
 {
-    await mongoose.connect('mongodb+srv://jaiyaxh:y3N2NOZcP8A5sm4H@todoapp.abmtknx.mongodb.net/todoTask?retryWrites=true&w=majority')
+    await mongoose.connect('mongodb+srv://jaiyaxh2:l7xEib1n7BbFN179@todoapp.abmtknx.mongodb.net/todoTask?retryWrites=true&w=majority')
 }
